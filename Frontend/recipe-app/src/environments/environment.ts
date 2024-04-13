a48dfa2a4219ba9ab1666d816eeb5e7ffc36209a
Mikael Lindeman <mikael.kalajalindeman@student.kyh.se>
2024-04-13 20:25:05 +0200

    2

--- Frontend/recipe-app/src/environments/environment.ts
+++ Frontend/recipe-app/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  api_url: 'https://u06-fullstack-recipe-app-mikaellindeman-2.onrender.com',
+  api_url: 'https://u06-fullstack-recipe-app-mikaellindeman-2.onrender.com/api',
 };
